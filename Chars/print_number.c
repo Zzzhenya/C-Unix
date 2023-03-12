ee11bce13044467844313ca54e8b2c3c18548d50
Shenya De Silva <shenya44@outlook.com>
2023-03-12 22:20:45 +0100

    Initial commit print_number.c

---
+++ Chars/print_number.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+void	print_number(int nb);
+void	print_digits(void);
+void	putcharacter(char c);
+
+int main(void)
+{
+	print_digits();
+	print_number(-42);
+	write (1, "\n", 1);
+	return (0);
+}
+
+
+/* Write 0 to 9 and line break */
+void	print_digits(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+	{
+		write (1, &i, 1);
+		i ++;
+	}
+	write (1, "\n", 1);
+}
+
+/* Write any number in int range */
+void	print_number(int nb)
+{
+	if (nb == -2147483648)
+		write (1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		putcharacter('-');
+		print_number(-nb);
+	}
+	else if (nb > 9)
+	{
+		print_number(nb / 10);
+		print_number(nb % 10);
+	}
+	else
+		putcharacter(nb + 48);
+}
+
+/* write character */
+void	putcharacter(char c)
+{
+	write (1, &c, 1);
+}
